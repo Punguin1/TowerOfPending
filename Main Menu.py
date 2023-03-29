0b50d89998492cb151aa31ead2432e7b0e92f8af
hackerrank <hackerrank.com>
2023-03-29 09:08:52 -0400

    Menu and not working screen melt

---
+++ Main Menu.py
@@ -0,0 +1,45 @@
+import pyxel
+
+
+class Menu():
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    
+    def draw(self):
+        pyxel.cls(0) # clear the screen with color index 0 (black)
+        pyxel.text(50, 30, "Main Menu", pyxel.COLOR_WHITE) # draw the title
+        pyxel.rect(50, 80, 80, 20, pyxel.COLOR_YELLOW) # draw a button
+        pyxel.text(60, 85, "Play Game", pyxel.COLOR_BLACK) # add text to the button
+    
+    def run(self):
+        pyxel.run(self.update, self.draw)
+    
+
+class Main:
+    def __init__(self):
+        self.draw_mainmenu = True
+        self.screen_width = 256
+        self.screen_height = 240
+        self.caption = "Gaming"
+        pyxel.init(self.screen_width, self.screen_height, self.caption)
+        # initialize game state here
+        self.menu = Menu()
+
+    def run(self):
+        self.menu.run()
+
+    def draw(self):
+        pyxel.cls(0) # clear the screen with color index 0 (black)
+        # draw game elements here
+
+    def update(self):
+        if self.draw_mainmenu:
+            self.menu.update()
+            if pyxel.btn(pyxel.KEY_ENTER):
+                self.draw_mainmenu = False
+        pass
+
+game = Main()
+game.run()
