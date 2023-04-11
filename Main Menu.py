1fc2bfb0694a656afe44e1eaf175e8fb5862117b
hackerrank <hackerrank.com>
2023-04-11 14:30:42 -0400

    Main menu but working

--- Main Menu.py
+++ Main Menu.py
@@ -1,25 +1,20 @@
 import pyxel
 
 
-# class Menu():
-#     def __init__(self):
-#         pass
-#     def update(self):
-#         pass
+class Draw_menu:
+    def __init__(self):
+        print("Menu Initialized")
+    def draw(self):
+        pyxel.text(50, 30, "Main Menu", pyxel.COLOR_WHITE) # draw the title
+        pyxel.rect(50, 80, 80, 20, pyxel.COLOR_YELLOW) # draw a button
+        pyxel.text(60, 85, "Play Game", pyxel.COLOR_BLACK) # add text to the button
+    def update(self):
+        print("test")
     
-#     def draw(self):
-#         pyxel.cls(0) # clear the screen with color index 0 (black)
-#         pyxel.text(50, 30, "Main Menu", pyxel.COLOR_WHITE) # draw the title
-#         pyxel.rect(50, 80, 80, 20, pyxel.COLOR_YELLOW) # draw a button
-#         pyxel.text(60, 85, "Play Game", pyxel.COLOR_BLACK) # add text to the button
-    
-#     def run(self):
-#         pyxel.run(self.update, self.draw)
-    
-
 class Main:
     def __init__(self):
-        draw_mainmenu = True
+        self.GAME_STATE = "menu"
+        self.draw_menu = Draw_menu()
         self.screen_width = 256
         self.screen_height = 240
         self.caption = "Gaming"
@@ -29,16 +24,18 @@ class Main:
         # initialize game state here
 
     def draw(self):
-        pyxel.cls(0) # clear the screen with color index 0 (black)
-        pyxel.rect(10, 10, 10, 10, 10)
-        if self.draw_mainmenu():
-            pyxel.text(20, 20, "Press E to die", 4)
-        # draw game elements here
+        if self.GAME_STATE == "menu":
+            self.draw_menu.draw()
+        else:
+            pyxel.cls(0)
+            pyxel.rect(10, 10, 10, 10, 10)
 
     def update(self):
-        if self.draw_mainmenu():
+        if self.GAME_STATE == "menu":
             if pyxel.btn(pyxel.KEY_E):
-                self.draw_mainmenu = False
+                self.GAME_STATE == "quit"
+        if self.GAME_STATE == "quit":
+            pyxel.quit()
         if pyxel.btn(pyxel.KEY_P):
             print("test")
 
