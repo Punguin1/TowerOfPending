daa6406e7bcb4a6ccebcdb1322ffffaacccd697c
hackerrank <hackerrank.com>
2023-04-11 13:51:04 -0400

    stuff>?????

--- Main Menu.py
+++ Main Menu.py
@@ -1,45 +1,45 @@
 import pyxel
 
 
-class Menu():
-    def __init__(self):
-        pass
-    def update(self):
-        pass
+# class Menu():
+#     def __init__(self):
+#         pass
+#     def update(self):
+#         pass
     
-    def draw(self):
-        pyxel.cls(0) # clear the screen with color index 0 (black)
-        pyxel.text(50, 30, "Main Menu", pyxel.COLOR_WHITE) # draw the title
-        pyxel.rect(50, 80, 80, 20, pyxel.COLOR_YELLOW) # draw a button
-        pyxel.text(60, 85, "Play Game", pyxel.COLOR_BLACK) # add text to the button
+#     def draw(self):
+#         pyxel.cls(0) # clear the screen with color index 0 (black)
+#         pyxel.text(50, 30, "Main Menu", pyxel.COLOR_WHITE) # draw the title
+#         pyxel.rect(50, 80, 80, 20, pyxel.COLOR_YELLOW) # draw a button
+#         pyxel.text(60, 85, "Play Game", pyxel.COLOR_BLACK) # add text to the button
     
-    def run(self):
-        pyxel.run(self.update, self.draw)
+#     def run(self):
+#         pyxel.run(self.update, self.draw)
     
 
 class Main:
     def __init__(self):
-        self.draw_mainmenu = True
+        draw_mainmenu = True
         self.screen_width = 256
         self.screen_height = 240
         self.caption = "Gaming"
-        pyxel.init(self.screen_width, self.screen_height, self.caption)
-        # initialize game state here
-        self.menu = Menu()
 
-    def run(self):
-        self.menu.run()
+        pyxel.init(self.screen_width, self.screen_height, self.caption)
+        pyxel.run(self.update, self.draw)
+        # initialize game state here
 
     def draw(self):
         pyxel.cls(0) # clear the screen with color index 0 (black)
+        pyxel.rect(10, 10, 10, 10, 10)
+        if self.draw_mainmenu():
+            pyxel.text(20, 20, "Press E to die", 4)
         # draw game elements here
 
     def update(self):
-        if self.draw_mainmenu:
-            self.menu.update()
-            if pyxel.btn(pyxel.KEY_ENTER):
+        if self.draw_mainmenu():
+            if pyxel.btn(pyxel.KEY_E):
                 self.draw_mainmenu = False
-        pass
+        if pyxel.btn(pyxel.KEY_P):
+            print("test")
 
-game = Main()
-game.run()
+Main()
